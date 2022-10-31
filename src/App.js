27cdc1ec194d57a34745d6941247ccdf99f0a35f
GGnagaraxx <giodelacruz05@gmail.com>
2022-10-31 17:20:36 +0800

    week4 task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    users: [
+      {
+        name: "James Bond",
+        age: 60
+      },
+      {
+        name: "Tom Cruise",
+        age: 50
+      },
+      {
+        name: "John Wick",
+        age: 45
+      },
+    ]
+  }
+
+  handleAddAge = (e) => {
+    let newUsersList = [
+      ...this.state.users,
+    ]
+
+    newUsersList[e.target.id].age += 1;
+
+    this.setState({newUsersList});
+  }
+
+  render(){
+    return(
+      <div className='page'>
+        <div className='container'>
+          <h1 className='title'>Users</h1>
+          {
+            this.state.users.map((userInfo, index)=>{
+              return(
+                <div key={index} className="card">
+                  <p className='bold'>{userInfo.name}</p>
+                  <div className='d-flex'>
+                    <p>Age: {userInfo.age}</p>
+                    <button
+                      id={index} 
+                      className="btn primary"
+                      onClick={this.handleAddAge}>+</button>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
